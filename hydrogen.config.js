3310a0742dddb1d07767ff3342535669de716b8d
KingAlex1997 <alex97l@hotmail.com>
2022-07-03 19:59:12 -0300

    first commit

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'https://fermax-shoes.myshopify.com/',
+    storefrontToken: '6e46fbce14d6086323c44cef696acee5',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
